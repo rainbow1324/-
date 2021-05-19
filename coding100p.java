294a73830966e82948876ca1e6328094e793ee6d
rainbow1324 <84062367+rainbow1324@users.noreply.github.com>
2021-05-19 12:14:19 +0900

    Add files via upload

---
+++ coding100p.java
@@ -0,0 +1,30 @@
+package kr.java.task;
+
+public class coding100p {
+
+	public static void main(String[] args) {
+		int score = 80;
+		
+		if(score >= 90) {
+			System.out.println("A"+"grade");
+		
+		}
+		else if(score >= 80) {
+			System.out.println("B"+"grade");
+		
+		}
+		else if(score >= 70) {
+			System.out.println("C"+"grade");
+		
+		}
+		else if(score >= 60) {
+			System.out.println("D"+"grade");
+		
+		}
+		else{
+			System.out.println("F"+"grade");
+		
+		}
+	}
+
+}
